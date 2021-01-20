d733e8dfd2d18662db6c0f29480eff06f954491d
Ewerton Klava <ekrodrigues@stefanini.com>
2021-01-20 15:56:11 -0300

    report

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,19 +1,39 @@
-require 'rspec'
-require 'page-object'
-require 'cucumber'
 require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'pry'
 require 'faker'
 
-Capybara.register_driver :selenium do |app|
-	Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 
+# end
+
+# Capybara.default_driver = :selenium
+# Capybara.default_max_wait_time = 15
+# Capybara.page.driver.browser.manage.window.maximize
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.default_max_wait_time = 15
 end
 
-Capybara.default_driver = :selenium
-Capybara.default_max_wait_time = 15
-Capybara.page.driver.browser.manage.window.maximize
 
-
+case ENV["BROWSER"]
+when "safari"
+  @driver = :selenium_safari
+when "chrome"
+  @driver = :selenium_chrome
+when "headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.add_argument "--headless"
+      opts.add_argument "--disable-gpu"
+      opts.add_argument "--no-sandbox"
+      opts.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+  @driver = :selenium_chrome
+else
+  puts "Invalid browser"
+end
