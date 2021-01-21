820452779e1f28269c73d17e6ba90d04b663138f
Ewerton Klava <ekrodrigues@stefanini.com>
2021-01-21 15:35:36 -0300

    multi browser

--- features/exercicio1.feature
+++ features/exercicio1.feature
@@ -9,5 +9,5 @@ Para a leitura de um artigo
 @1
 Cenario: Abrir reportagem Stefanini
 Dado que estou na pagina inicial da Stefanini
-# Quando clicar em Blog Carreiras
-# Entao verificar o artigo 'As 4 certificações Scrum mais valorizadas no mercado' é exibido
+Quando clicar em Blog Carreiras
+Entao verificar o artigo 'As 4 certificações Scrum mais valorizadas no mercado' é exibido
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,26 +4,35 @@ require 'selenium-webdriver'
 require 'pry'
 require 'faker'
 
-# Capybara.register_driver :selenium do |app|
-#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
-
-# end
-
-# Capybara.default_driver = :selenium
-# Capybara.default_max_wait_time = 15
-# Capybara.page.driver.browser.manage.window.maximize
-Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
-  config.default_max_wait_time = 15
-end
-
 
 case ENV["BROWSER"]
-when "safari"
-  @driver = :selenium_safari
-when "chrome"
-  @driver = :selenium_chrome
-when "headless"
+  when "chrome"
+    Capybara.default_driver = :chrome
+    Capybara.register_driver :chrome do |app|
+    options = {
+    :js_errors => false,
+    :timeout => 360,
+    :debug => false,
+    :inspector => false,
+    }
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+  Capybara.page.driver.browser.manage.window.maximize
+ 
+  when "safari"
+    Capybara.default_driver = :safari
+    Capybara.register_driver :safari do |app|
+    options = {
+    :js_errors => false,
+    :timeout => 360,
+    :debug => false,
+    :inspector => false,
+    }
+    Capybara::Selenium::Driver.new(app, :browser => :safari)
+  end
+  Capybara.page.driver.browser.manage.window.maximize
+ 
+  when "headless"
   Capybara.register_driver :selenium_chrome_headless do |app|
     chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
       opts.add_argument "--headless"
@@ -33,7 +42,7 @@ when "headless"
     end
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
   end
-  @driver = :selenium_chrome
-else
-  puts "Invalid browser"
-end
+  @driver = :selenium_chrome_headless
+  else
+    puts "Invalid browser"
+  end
